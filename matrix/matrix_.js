00a065b73d52e6af229327281b1dd58e3ddd566c
Hoang Nhat <hoangnhat2911@gmail.com>
2014-12-15 23:36:19 -0500

    add and render matrices now works

--- matrix/matrix_.js
+++ matrix/matrix_.js
@@ -1,57 +1,60 @@
-// simple-todos.js
-Tasks = new Mongo.Collection("matrix");
+Matrices = new Mongo.Collection("matrix");
 
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     matrix: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      return Matrices.find({});
     }
   });
 
   // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
   Template.body.events({
     "submit .new-task": function (event) {
-      event.preventDefault();
 
       var r = event.target.row.value;
           c = event.target.col.value;
-      var multiply = r * c;
-      var m = [];
-      for (var i = 0; i < r * c; i++){
-      	m[i] = i;
-      }
-      Tasks.insert({
+
+      Matrices.insert({
         row: r,
-        col: c,
-        multiply: multiply,
-        matrix_array: m,
+        col: c
       });
 
       // Clear form
       event.target.row.value = "";
       event.target.col.value = "";
 
-
       // Prevent default form submit
       return false;
     }
   });
+  Template.createInput.helpers({
+    addTable: function() {
+      var matrix = document.getElementById("newmatrix");
 
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
+      var table = document.createElement('TABLE');
+      table.border='1';
+      
+      var tableBody = document.createElement('TBODY');
+      table.appendChild(tableBody);
+        
+      for (var i = 0; i < this.row; i++){
+         var tr = document.createElement('TR');
+         tableBody.appendChild(tr);
+         
+         for (var j = 0; j < this.col; j++){
+             var td = document.createElement('TD');
+             td.width='5';
+             td.appendChild(document.createTextNode(i + "," + j));
+             tr.appendChild(td);
+         }
+      }
+      matrix.appendChild(table);
     }
   });
-
-  Template.task.helpers({
-    "hellothere": function() {
-      console.log("The result of " + this.multiply + " mod " + this.matrix_array + " is " + (this.multiply % (this.matrix_array + 1) == 0));
-      return (this.multiply % (this.matrix_array + 1) == 0);
+  Template.createInput.events({
+    "click .delete": function () {
+      Matrices.remove(this._id);
     }
   });
 }
